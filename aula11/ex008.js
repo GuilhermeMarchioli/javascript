9ae73887d38d805f02431321f4dd7efca9f19f7b
Alexandre Marchioli <marchioliprogrammer@gmail.com>
2025-01-20 17:36:36 -0300

    exercicios javascript constantes

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 72.6
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+
+if (vel > 80) {//Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida de 80km/h. MULTADO!`)
+}
+
+console.log(`Use sempre o cinto de segurança, e se beber não dirija!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasileiro'
+console.log('Comprar algo no RJ:')
+
+if (pais == 'Brasil') {
+    console.log('Picolé ta 2 real chefia.')
+} else {
+    console.log('200 Pila na mão alemão, pa gringo é maix carou')
+}
