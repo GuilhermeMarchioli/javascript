690b6ef680eaf79b73e106b5a423dcd453e8d7de
Alexandre Marchioli <marchioliprogrammer@gmail.com>
2025-02-03 17:40:26 -0300

    desafio

---
+++ desafio05/script.js
@@ -0,0 +1,28 @@
+let num = document.getElementById('txtnum')
+let lista = document.getElementById('select')
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function add() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('Tudo ok') 
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+}
