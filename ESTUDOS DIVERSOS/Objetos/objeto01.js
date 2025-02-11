2199e5cbbbec145d959f13ebbc36a29f9771171f
Alexandre Marchioli <marchioliprogrammer@gmail.com>
2025-02-11 15:23:42 -0300

    objetos

---
+++ ESTUDOS DIVERSOS/Objetos/objeto01.js
@@ -0,0 +1,8 @@
+const dadosUser = {
+    nome: 'Guilherme Marchioli',
+    idade: 27,
+    endereco: 'Rua rio tibagi, 829',
+    cpf: '123.456.789-10'
+}
+
+console.log(`O usuário possuí esses dados: nome: ${dadosUser.nome}; idade: ${dadosUser.idade}; endereço: ${dadosUser.endereco}; cpf: ${dadosUser.cpf}`)
