62a0ec22af1bc0cb2ab086e57f740347ea25f6b7
Guilherme da Silva Marchioli <marchioliprogrammer@gmail.com>
2025-06-16 16:31:28 -0300

    tentando estudar nessa porra de node

---
+++ ESTUDOS DIVERSOS/Exercícios/ex002/node_bf1fb3963f3ba.tmp.js
@@ -0,0 +1,10 @@
+const data = [
+  { nome: 'Matheus', idade: 31, salario: 2000, cargo: 'Marketing' },
+  { nome: 'Ana', idade: 25, salario: 3000, cargo: 'Designer' },
+  { nome: 'Carlos', idade: 40, salario: 5000, cargo: 'Contaveis' },
+  { nome: 'Felipe', idade: 23, salario: 1700, cargo: 'Secretario' },
+  { nome: 'Jessica', idade: 19, salario: 1600, cargo: 'Estagiário'},
+  { nome: 'Tobias', idade: 26, salario: 7000, cargo: 'Dev' },
+];
+
+console.log(data)
